dc9bbfdca2dbe5c0a95f7837bebd8d9dcfa61b35
Crowdbotics <team@crowdbotics.com>
2023-06-14 17:01:40 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/trainingapp_kanyins_41483/MainActivity.java
@@ -0,0 +1,15 @@
+package com.trainingapp_kanyins_41483;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "trainingapp_kanyins_41483";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "trainingapp-kanyins-41483",
+  url: "https://trainingapp-kanyins-41483.botics.co",
+  api: "https://trainingapp-kanyins-41483.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
